8995407ea35b791571de872d542a5005415f7d15
Tanmay0215 <tanmaygupta.0215@gmail.com>
2025-09-25 16:20:59 +0530

    add mongodb

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
+connectDB();
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
